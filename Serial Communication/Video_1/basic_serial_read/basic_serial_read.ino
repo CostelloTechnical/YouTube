dfcbfb469a7aae461e1d79e1ca1cfa55791d015f
ThatsClass <accounts.useful@protonmail.com>
2024-07-14 13:58:15 +0100

    Adding the Arduino code for the YT basic_serial_read.

---
+++ Serial Communication/Video_1/basic_serial_read/basic_serial_read.ino
@@ -0,0 +1,33 @@
+const char startCharacter = '<';
+const char endCharacter ='>';
+const uint8_t maxCharacters = 50;
+char receivedCharacters[maxCharacters];
+bool receivingData = false;
+uint8_t receivedCharcterIndex = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    char receivedCharacter = Serial.read();
+
+    if(receivedCharacter == startCharacter){
+      receivingData = true;
+    }
+    else if(receivingData == true && receivedCharacter != endCharacter){
+      receivedCharacters[receivedCharcterIndex] = receivedCharacter;
+      receivedCharcterIndex ++;
+    }
+    else if(receivingData == true && receivedCharacter == endCharacter){
+      receivedCharacters[receivedCharcterIndex] = '\0';
+      for(uint8_t i = 0; i < receivedCharcterIndex; i++){
+        Serial.write(receivedCharacters[i]);
+      }
+      receivedCharcterIndex = 0;
+      receivingData = false;
+      Serial.write("\n\r");
+    }
+  }
+}
