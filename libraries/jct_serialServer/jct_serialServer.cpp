8b41cd17e255c840aa747b3db296b5623a60fef0
ThatsClass <accounts.useful@protonmail.com>
2025-08-07 22:53:11 +0100

    Creating the files for a new library. It'll be a serial server for an arduino with an Ethernet shield.

---
+++ libraries/jct_serialServer/jct_serialServer.cpp
@@ -0,0 +1,1 @@
+#include "jct_serialServer"
---
+++ libraries/jct_serialServer/jct_serialServer.h
@@ -0,0 +1,14 @@
+#ifndef JCT_SERIALSERVER_H
+#define JCT_SERIALSERVER_H
+#include <Arduino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+class jctSerialServer
+{
+    public:
+
+    private:
+
+};
+#endif
